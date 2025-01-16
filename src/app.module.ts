9aa81b286fde1bf384edb9a899a34701bced1b67
Jack Sari <janasarii@gmail.com>
2025-01-16 15:30:10 -0500

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Root@1234',
-      database: 'play',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
